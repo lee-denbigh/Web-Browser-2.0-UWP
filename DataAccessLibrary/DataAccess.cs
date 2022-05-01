c0c5c62b4a3ef2f5cedf7f1ea1423e6d0a676d07
Lee denbigh <denbigh1@live.com>
2022-05-01 14:19:56 +0100

    Part 2 Finished

---
+++ DataAccessLibrary/DataAccess.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+using Windows.Storage;
+
+namespace DataAccessLibrary
+{
+    public static class DataAccess
+    {
+        static string dbPath = "webbrowser2.db";
+
+        // This will create the database and tables.
+        public static async void InitialiseDatabase()
+        {
+            await ApplicationData.Current.LocalFolder.CreateFileAsync(dbPath, CreationCollisionOption.OpenIfExists);
+            string dbpath = Path.Combine(ApplicationData.Current.LocalFolder.Path, dbPath);
+
+            using (SqliteConnection conn =
+                new SqliteConnection($"Filename={dbpath}"))
+            {
+                conn.Open();
+
+                String searchTermsTableCommand = "CREATE TABLE IF NOT " +
+                    "EXISTS searchterms (searchtermID INTEGER PRIMARY KEY," +
+                    "searchterm VARCHAR(2048) NOT NULL)";
+
+                SqliteCommand createTable = new SqliteCommand(searchTermsTableCommand, conn);
+
+                createTable.ExecuteReader();
+            }
+        }
+    }
+}
--- Web Browser 2.0 UWP/App.xaml.cs
+++ Web Browser 2.0 UWP/App.xaml.cs
@@ -14,6 +14,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using DataAccessLibrary;
 
 namespace Web_Browser_2._0_UWP
 {
@@ -30,6 +31,8 @@ namespace Web_Browser_2._0_UWP
         {
             this.InitializeComponent();
             this.Suspending += OnSuspending;
+
+            DataAccess.InitialiseDatabase();
         }
 
         /// <summary>
